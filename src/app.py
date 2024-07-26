d3587d1b488e61cec483882f293edc6f618b1ab3
Sebastian Solano <32124063+sbs29@users.noreply.github.com>
2024-07-26 09:48:06 +0000

    endpoints user finished

--- src/app.py
+++ src/app.py
@@ -36,14 +36,77 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user/', methods=['GET'])
+def get_all_user():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users_query = User.query.all()
+    users_list = list(map(lambda user: user.serialize(),users_query))
 
-    return jsonify(response_body), 200
+    return jsonify(users_list), 200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return jsonify({"info":"Not Found"}), 404
+    
+    print(user.favorites_planets)
+    print(user.favorites_people)
+
+    response = user.serialize()
+    response["favorites_planets"] = list(
+        map(lambda planet:planet.serialize(), user.favorites_planets))
+    response["favorites_people"] = list(
+        map(lambda people:people.serialize(), user.favorites_people))
+
+    return jsonify(response), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    user_body = request.get_json()
+    user_db = User(first_name = user_body["first_name"],
+                   last_name = user_body["last_name"],
+                   email = user_body["email"],
+                   password = user_body["password"],
+                   is_active = user_body["is_active"])
+    db.session.add(user_db)
+    db.session.commit()
+    return jsonify(user_db.serialize()), 201
+
+@app.route('/user/<int:user_id>', methods=['PATCH'])
+def update_user(user_id):
+
+    user_db = User.query.filter_by(id=user_id).first()
+    if user_db is None:
+        return jsonify({"info":"Not Found"}), 404
+    user_body = request.get_json()
+    
+    if "first_name" in user_body:
+        user_db.first_name = user_body["first_name"]
+    if "last_name" in user_body:
+        user_db.first_name = user_body["last_name"]
+    if "email" in user_body:
+        user_db.first_name = user_body["email"]
+    if "password" in user_body:
+        user_db.first_name = user_body["password"]
+    if "is_active" in user_body:
+        user_db.first_name = user_body["is_active"]
+
+    db.session.add(user_db)
+    db.session.commit()
+    return jsonify(user_db.serialize())
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+
+    user_db = User.query.filter_by(id=user_id).first()
+    if user_db is None:
+        return jsonify({"info":"Not Found"}), 404
+    db.session.delete(user_db)
+    db.session.commit()
+    return jsonify({"info":"User deleted"})
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -15,5 +17,104 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "full_name": self.first_name + ' ' + self.last_name
             # do not serialize the password, its a security breach
+        }
+    
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    gender = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+    hair_color = db.Column(db.String(100), nullable=False)
+    skin_color = db.Column(db.String(150), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year
+        }
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "diameter" : self.diameter,
+            "rotation_period" : self.rotation_period,
+            "orbital_period" : self.orbital_period,
+            "gravity" : self.gravity,
+            "population" : self.population,
+            "climate" : self.climate,
+            "terrain" : self.terrain,
+            "surface_water" : self.surface_water
+        }
+
+
+class FavoritePlanets(db.Model):
+    __tablename__ = 'favorites_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    user = db.relationship(User, backref="favorites_planets")
+    planets = db.relationship(Planets)
+
+    def __repr__(self):
+        return f"${self.user.first_name} likes ${self.planet.name}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.first_name,
+            "planet": self.planet.name
+        }
+
+
+class FavoritePeople(db.Model):
+    __tablename__ = 'favorites_people'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    user = db.relationship(User, backref="favorites_people")
+    people = db.relationship(People)
+
+    def __repr__(self):
+        return f"${self.user.first_name} likes ${self.people.name}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.first_name,
+            "people": self.people.name
         }
