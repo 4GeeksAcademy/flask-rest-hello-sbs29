39d3fbff7ce9084539df1f3d4ae093ed731d2312
Sebastian Solano <32124063+sbs29@users.noreply.github.com>
2024-08-02 16:55:37 +0000

    In process...

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(120), unique=False, nullable=False)
     last_name = db.Column(db.String(120), unique=False, nullable=False)
@@ -116,5 +117,6 @@ class FavoritePeople(db.Model):
         return {
             "id": self.id,
             "user": self.user.first_name,
-            "people": self.people.name
+            "people": self.people.name,
+            "people_image": f"https://starwars-visualguide.com/assets/img/characters/{self.people_id}.jpg"
         }
